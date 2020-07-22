1269af7c0693ee1df59ba1b43338ff4cc3454b75
Sean Marquez <capsulecorplab@gmail.com>
2020-07-21 23:43:15 -0700

    replace Dockerfile w/ docker image from DockerHub
    Closes #31

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$(docker images -q hugo-asciidoctor-plantuml:Dockerfile 2>/dev/null)" ==  "" ]]; then
-    docker build -t hugo-asciidoctor-plantuml:Dockerfile .
+if [[ "$(docker images -q capsulecorplab/hugo-asciidoctor-plantuml 2>/dev/null)" ==  "" ]]; then
+    docker build -t capsulecorplab/hugo-asciidoctor-plantuml .
 fi
 if [ ! -r ./node_modules ]; then
-    docker run --rm --volume $PWD:/src -w "/src" hugo-asciidoctor-plantuml:Dockerfile bash -c "npm ci"
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml bash -c "npm ci"
 fi
 if [ ! -r ./public ]; then
-    docker run --rm --volume $PWD:/src -w "/src" hugo-asciidoctor-plantuml:Dockerfile bash -c "hugo --minify -v --destination public"
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml bash -c "hugo --minify -v --destination public"
 fi
