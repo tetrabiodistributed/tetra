d4dcf41ed2256a27d68e183a5ce8c41d0e4479b5
Sean Marquez <capsulecorplab@gmail.com>
2020-08-14 10:40:01 -0700

    remove unused commands for building docker image (#53)

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "$(docker images -q capsulecorplab/hugo-asciidoctor-plantuml 2>/dev/null)" ==  "" ]]; then
-    docker build -t capsulecorplab/hugo-asciidoctor-plantuml .
-fi
 if [ ! -r ./node_modules ]; then
     docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml bash -c "npm ci"
 fi
